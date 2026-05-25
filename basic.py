9fc2b7fbf438337a96edf0b91e8b50970e83d820
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-05-25 12:32:18 +0600

    Added new files

--- basic.py
+++ basic.py
@@ -1,4 +1,8 @@
-def add(a,b):
-    return a+b
-fall=add(5,6)
-print(fall)
+print("welcome to python prsctice ")
+english=float(input("enter your english mark :"))
+math=float(input("enter your math mark :"))
+bangla=float(input("enter your bangla mark :"))
+total_mark=english+math+bangla
+
+avg=total_mark/3
+print(f"total mark is {total_mark} and avg is {avg}")
