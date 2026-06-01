60d95b59184be00d8887cf0f04fe55e62adee825
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-06-01 22:10:48 +0600

    daily practice

--- prac.py
+++ prac.py
@@ -242,3 +242,14 @@ for i in range(3, 0, -1):
     for j in range(i):
         print("*", end=" ")
     print()
+
+print("welcome to result system")
+name=input("Enter your name : ")
+math=float(input("enter your math mark : "))
+bangla=float(input("enter your bangla mark : "))
+english=float(input("enter your english mark : "))
+total=math+bangla+english
+average=total/3
+print("Name : ",name)
+print("Total mark : ",total)
+print("Average mark : ",average)
