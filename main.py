595a9f6e25645be20dc89b544e8b8d95eafc67ef
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-06-01 22:30:32 +0600

    Added new files

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
 print("welcome to result system")
-name=input("Enter your name : ")
-math=float(input("enter your math mark : "))
-bangla=float(input("enter your bangla mark : "))
-english=float(input("enter your english mark : "))
-total=math+bangla+english
-average=total/3
-print("Name : ",name)
-print("Total mark : ",total)
-print("Average mark : ",average)
