ba11ef13cd4c9b98233a66b45163728f01c636d5
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-06-11 00:34:20 +0600

    Added new files

---
+++ day6.py
@@ -0,0 +1,75 @@
+from os import remove
+listt=[1,2,3,4,5,6]
+print(listt)
+for i in listt:
+  print(i)
+print(listt[1::2])
+listt.append(7)
+print(listt)
+listt.insert(7,8)
+print(listt)
+listt.remove(8)
+print(listt)
+listt.pop(6)
+print(listt)
+listt.clear()
+print(listt)
+tupple=(1,2,3,4,"rimel","tanjil")
+print(tupple)
+for i in tupple:
+  print(i)
+
+dic={
+    "name":"rimel",
+    "age":23
+}
+print(dic)
+for key in dic:
+  print(key)
+
+for value in dic.values():
+  print(value)
+
+for key,value in dic.items():
+  print(key,value)
+print(dic.get("name"))
+dic["name"]="tanjil"
+print(dic["name"])
+dic.update({"name":"rimel","age":24})
+print(dic)
+del dic["name"]
+print(dic)
+dic.pop("age")
+print(dic)
+dic={
+    "s1":{"name":"rimel","age":23},
+    "s2":{"name":'tanjil',"age":23}
+}
+print(dic)
+dic["s3"]={"name":'rahim',"age":25}
+print(dic)
+for id,info in dic.items():
+  print(id)
+  for key,value in info.items():
+    print(key,value)
+
+dic.clear()
+print(dic)
+
+sett={1,2,3,4,5,6}
+print(sett)
+for i in sett:
+  print(i)
+
+sett.add(8)
+print(sett)
+sett.update({9,10,11,12,13})
+print(sett)
+sett.remove(13)
+print(sett)
+sett.discard(13)
+print(sett)
+sett.pop()
+print(sett)
+sett.clear()
+print(sett)
