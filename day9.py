4ba81eeebea9e0fe0ae72d86f60a161a98d05184
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-06-21 23:23:40 +0600

    Added new files

---
+++ day9.py
@@ -0,0 +1,15 @@
+listt=[1,2,3,4,5,6]
+print(listt)
+for i in listt:
+  print(i)
+
+listt.append(7)
+print(listt)
+listt.insert(7,8)
+print(listt)
+listt.remove(8)
+print(listt)
+listt.pop(6)
+print(listt)
+listt.clear()
+print(listt)
