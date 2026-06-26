f1669e2509c6204661980ddc43f22f011797bc0e
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-06-27 00:06:53 +0600

    update something

--- day11.py
+++ day11.py
@@ -3,3 +3,14 @@ if mark>=50:
     print("pass")
 else:
     print("fail")
+
+
+dic={}
+while True:
+    key=input("enter your key :")
+    if key=="end":
+        break
+    value=input("enter your value :")
+    dic[key]=value
+for key,value in dic.items():
+    print(key,value)
