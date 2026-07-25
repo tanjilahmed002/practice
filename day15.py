8603fb47f18001384678524ad02daff42567d145
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-07-26 00:48:55 +0600

    update something

--- day15.py
+++ day15.py
@@ -1,5 +1,16 @@
-listt=[1,2,3,4,5]
+listt=[1,2,3,4,5,6]
+print(listt)
+for i in listt:
+  print(i)
+listt.append(7)
+print(listt)
+listt.insert(7,8)
+print(listt)
+print(listt[-6:-1])
+listt.remove(7)
+print(listt)
+listt.pop(6)
+print(listt)
+listt.clear()
 print(listt)
 
-listt.append(6)
-print(listt)
