c5c031274961bea75f8d7d0bcacba25d7b29b95d
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-08-07 20:32:24 +0600

    Added new files

---
+++ day18.py
@@ -0,0 +1,12 @@
+import random
+print(random.random())
+
+print(random.randint(1000,9999))
+print(random.randrange(100,999,10))
+
+fruit=["apple","mango","banana",1,2,3,4,5,6,7]
+print(random.sample(fruit,2))
+print(random.choice(fruit))
+
+otp=random.randint(1000,9999)
+print("here is your otp :",otp)
