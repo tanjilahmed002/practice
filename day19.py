988fa542e103c95f5eaff1ad7a0777459b84e48d
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-08-11 00:00:12 +0600

    Added new files

---
+++ day19.py
@@ -0,0 +1,12 @@
+#student information
+
+name=input("enter your student name :")
+id=input("enter your student id :")
+department=input("enter your department :")
+semester=input("enter your semester :")
+age=int(input("enter your age :"))
+print(f"Name:{name}")
+print("ID :",id)
+print("Department :",department)
+print("semester :",semester)
+print("Age :",age)
