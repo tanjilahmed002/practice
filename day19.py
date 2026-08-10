f3c8f17063fad738d18308f1aacf17f6dc3c0cec
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-08-11 00:47:00 +0600

    update something

--- day19.py
+++ day19.py
@@ -1,52 +1,52 @@
 #student information
 
-# name=input("enter your student name :")
-# id=input("enter your student id :")
-# department=input("enter your department :")
-# semester=input("enter your semester :")
-# age=int(input("enter your age :"))
-# print(f"Name:{name}")
-# print("ID :",id)
-# print("Department :",department)
-# print("semester :",semester)
-# print("Age :",age)
+name=input("enter your student name :")
+id=input("enter your student id :")
+department=input("enter your department :")
+semester=input("enter your semester :")
+age=int(input("enter your age :"))
+print(f"Name:{name}")
+print("ID :",id)
+print("Department :",department)
+print("semester :",semester)
+print("Age :",age)
 
 
 
 #simple calculator
 
-# while True:
-#     print(" 1.Addition")
-#     print(" 2.Substraction")
-#     print(" 3.Multiplication")
-#     print(" 4.Division")
+while True:
+    print(" 1.Addition")
+    print(" 2.Substraction")
+    print(" 3.Multiplication")
+    print(" 4.Division")
 
     
-#     inn=int(input("enter your choice & type 0 for end the loop :"))
-#     if inn==0:
-#         break
-#     if inn==1:
-#         num1=float(input("enter your num1 :"))
-#         num2=float(input("enter your num2 :"))
-#         Addition=num1+num2
-#         print("Addition :",Addition)
-#     elif inn==2:
-#         num3=float(input("enter your num3 :"))
-#         num4=float(input("enter your num4 :"))
-#         Substraction=num3-num4
-#         print("substraction :",Substraction)
-#     elif inn==3:
-#         num5=float(input("enter your num5 :"))
-#         num6=float(input("enter your num6 :"))
-#         Multiplication=num5*num6
-#         print("Multiplication :",Multiplication)
-#     elif inn==4:
-#         num7=float(input("enter your num7 :"))
-#         num8=float(input("enter your num8 :"))
-#         Division=num7/num8
-#         print("Division :",Division)
-#     else:
-#         print("invalid choice!try again.")
+    inn=int(input("enter your choice & type 0 for end the loop :"))
+    if inn==0:
+        break
+    if inn==1:
+        num1=float(input("enter your num1 :"))
+        num2=float(input("enter your num2 :"))
+        Addition=num1+num2
+        print("Addition :",Addition)
+    elif inn==2:
+        num3=float(input("enter your num3 :"))
+        num4=float(input("enter your num4 :"))
+        Substraction=num3-num4
+        print("substraction :",Substraction)
+    elif inn==3:
+        num5=float(input("enter your num5 :"))
+        num6=float(input("enter your num6 :"))
+        Multiplication=num5*num6
+        print("Multiplication :",Multiplication)
+    elif inn==4:
+        num7=float(input("enter your num7 :"))
+        num8=float(input("enter your num8 :"))
+        Division=num7/num8
+        print("Division :",Division)
+    else:
+        print("invalid choice!try again.")
 
 
 
