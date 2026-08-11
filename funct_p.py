d2e627537376dd1108f336644681d4eb3b0e73b8
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-08-11 17:56:17 +0600

    update something

--- funct_p.py
+++ funct_p.py
@@ -16,3 +16,22 @@ def function_name():
     return 100
 
 print(function_name())
+
+
+def info(name,age):
+    print(name,age)
+info("rimel",23)
+
+def student(name,age):
+    print(name,age)
+student(name="rimel ahmed",age=24)
+
+def square(x):
+    return x*x
+ss=square(4)
+print(ss)
+
+def mul(a,b):
+    return a+b,a-b,a*b,a/b
+w,x,y,z=mul(5,4)
+print(w,x,y,z)
