0b75625eb6db21873066b6535e88fd55696fd6c9
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-08-11 17:35:17 +0600

    Added new files

--- funct_p.py
+++ funct_p.py
@@ -5,3 +5,14 @@ function_name()
 def function_name(name):
     print("hello",name)
 function_name("rimel")
+
+
+def function_name(a,b):
+    return a+b
+add=function_name(2,3)
+print(add)
+
+def function_name():
+    return 100
+
+print(function_name())
