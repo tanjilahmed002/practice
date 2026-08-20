efb69ee000ef58975c37b9ce6345dc634f9771bb
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-08-20 23:31:33 +0600

    Added new files

--- day22.py
+++ day22.py
@@ -11,3 +11,11 @@ def function_name(a,b):
     return a+b
 add=function_name(3,2)
 print(add)
+
+def function_name():
+    return 100
+print(function_name())
+
+def function_name(name,age):
+    print(name,age)
+function_name(name="rimel ahmed",age=23)
