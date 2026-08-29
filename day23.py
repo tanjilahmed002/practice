0bac3d5a315d670068b6e0ade2e0cc2bf036115a
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-08-29 19:14:14 +0600

    update something

--- day23.py
+++ day23.py
@@ -48,4 +48,119 @@ for value in dic.values():
 
 for key,value in dic.items():
     print(key,value)
-    
+
+
+del dic["age"]
+print(dic)
+
+dic.pop("name")
+print(dic)
+
+dic["name"]="tanjil ahmed"
+print(dic)
+dic.pop("name")
+print(dic)
+
+dic.update({"name":"tanjil","age":23})
+print(dic)
+dic.popitem()
+print(dic)
+
+dic.clear()
+print(dic)
+
+dic={
+    "s1":{"name":"rimel","age":23},
+    "s2":{"name":"tanjil","age":24}
+}
+print(dic)
+
+print(dic["s1"]["name"])
+
+dic["s1"]["name"]="rimel ahmed"
+print(dic["s1"]["name"])
+
+dic.update({"s1":{"name":"rimel","age":25}})
+print(dic)
+
+dic.popitem()
+print(dic)
+del dic["s1"]
+print(dic)
+
+sett={1,2,3,4,5}
+sett.add(6)
+print(sett)
+
+sett.update({7,8,9,10,11,12,13})
+print(sett)
+
+sett.remove(11)
+print(sett)
+sett.discard(11)
+print(sett)
+
+sett.pop()
+print(sett)
+
+sett.clear()
+print(sett)
+
+fruits=["rimel",'apple',"banana","mango"]
+print('apple' in fruits)
+if "apple" in fruits:
+    print("found")
+else:
+    print("not found")
+
+
+num=[1,2,3,4,5]
+for i in num:
+    if i==2:
+        print("found")
+    else:
+        print("not found")
+
+
+while True:
+    search=input("enter your choice & type end for stop this loop :").lower()
+    if search=="end":
+        break
+    if search in fruits:
+        print("found")
+    else:
+        print("not found")
+
+
+counnt=0
+for i in num:
+    if i==1:
+        counnt=counnt+1
+print(counnt)
+
+dic={}
+while True:
+    key=input("enter your key :")
+    if key=="end":
+        break
+    value=input("enter your value :")
+    dic[key]=value
+for key,value in dic.items():
+    print(key,value)
+
+import random
+print(random.randint(1000,9999))
+print(random.random())
+print(random.randrange(100,999,10))
+print(random.choice(fruits))
+print(random.sample(fruits,2))
+
+otp=random.randint(1000,9999)
+print("your otp is here :",otp)
+
+
+class Car:
+    def __init__(self):
+        self.name="rimel ahmed"
+c=Car()
+print(c.name)
