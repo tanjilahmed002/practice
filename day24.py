29b4c14940dacdf4bf3d8cf60c66fddea16de7ca
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-08-31 21:46:37 +0600

    Added new files

---
+++ day24.py
@@ -0,0 +1,18 @@
+def function_name():
+    print("hello")
+function_name()
+
+
+def function_name(name):
+    print("hello",name)
+function_name("rimel")
+
+
+def function_name(a,b):
+    return a+b
+add=function_name(4,2)
+print(add)
+
+def sq():
+    return 200
+sq()
