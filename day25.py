39c3af1580836555c158ed4513d2fabe9051ea17
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-09-01 19:35:45 +0600

    Added new files

--- day25.py
+++ day25.py
@@ -3,3 +3,24 @@ class Car:
         self.name="rimel"
 c=Car()
 print(c.name)
+
+
+class Bike:
+    def __init__(self):
+        self._name="rimel"
+
+class Bike:
+    def __init__(self):
+        self.__name="rimel ahmed"
+
+
+from abc import ABC,abstractmethod
+class Car(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+class Bike(Car):
+    def sound(self):
+        print("bike is not a car")
+b=Bike()
+b.sound()
