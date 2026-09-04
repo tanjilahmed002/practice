a444eb858a0c782593dfd51359e3514bd05ccc3e
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-09-04 19:08:07 +0600

    Added new files

--- data.py
+++ data.py
@@ -3,3 +3,12 @@ if "apple" in fruits:
     print("found")
 else:
     print("not found")
+
+print("apple" in fruits)
+
+num=[1,2,3,4,5,6,7,8,9,1,1,2,1,4,5]
+for i in num: 
+    if i==1:
+        print("found")
+    else:
+        print("not found")
