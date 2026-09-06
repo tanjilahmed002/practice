09908ec4b36e31ed40d235d470c2ff0821764a93
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-09-06 20:41:05 +0600

    Added new files

--- day26.py
+++ day26.py
@@ -5,3 +5,11 @@ if "apple" in fruit:
     print("found")
 else:
     print("not found")
+
+
+num=[1,2,3,4,5,6,7,8,9,1,2,1,1,2,3,4,5,4,1,2]
+for i in num:
+    if i==1:
+        print("found")
+    else:
+        print("not found")
