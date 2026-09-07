ea410532e522efd9c344b0c85dc06f78d187f772
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-09-07 21:19:42 +0600

    Added new files

--- day27.py
+++ day27.py
@@ -1,3 +1,9 @@
 num=[1,2,3,4,5,6,7,1,2,1,1,2,4,5,3]
 fruit=["apple",'mango',"banana"]
 print(num.count(1))
+
+print(fruit.index("banana"))
+
+for i in range(len(num)):
+    if num[i]==1:
+        print("found at :",i)
