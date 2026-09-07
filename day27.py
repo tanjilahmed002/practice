a835541ff730d38b24a6707980db07bc4d468400
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-09-07 21:29:28 +0600

    Added new files

--- day27.py
+++ day27.py
@@ -14,3 +14,20 @@ for i in num:
         unique.append(i)
     for j in unique:
         print(i,"appears at :",num.count(i))
+
+
+
+class Bank:
+    def __init__(self):
+        self.__balance=500
+    def get_balance(self):
+        return self.__balance
+    def set_balance(self,amount):
+        if amount>0:
+            self.__balance=amount
+        else:
+            print("invalid balance")
+b=Bank()
+print(b.get_balance())
+b.set_balance(1000)
+print(b.get_balance())
