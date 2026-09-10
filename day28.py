432ba021ccfdba4d46950873f25b3f5eb462fa54
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-09-10 23:37:57 +0600

    update something

--- day28.py
+++ day28.py
@@ -40,3 +40,26 @@ print(dic.get("age"))
 
 dic["name"]="rimel ahmed"
 print(dic["name"])
+
+print(dic.update({"name":"rimel","age":24,"mark":77}))
+print(dic)
+
+del dic["age"]
+print(dic)
+
+dic.pop("name")
+print(dic)
+dic.popitem()
+print(dic)
+
+dic={
+    "s1":{"name":"rimel","age":23},
+    "s2":{"name":"tanjil","age":24}
+}
+
+print(dic)
+
+for i,j in dic.items():
+    print(i)
+    for key,value in j.items():
+        print(key,value)
