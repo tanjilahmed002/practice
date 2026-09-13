ca5deb6aa2e291185ba8c4e0859a5290f08d785b
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-09-14 00:39:22 +0600

    update something

--- day29.py
+++ day29.py
@@ -2,3 +2,14 @@ fruit=['mango',"apple","banana"]
 print("apple" in fruit)
 if "apple" in fruit:
     print("found")
+num=[1,2,3,4,5,6,7,1,2,1,2,2]
+for i in num:
+    if i==2:
+        print("found")
+    else:
+        print("not found")
+
+
+print(num.count(2))
+
+print(num.index(2))
