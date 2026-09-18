763140901f3f667356f70b38a5afba9d5c8ee074
Tanjil Ahmed <tanjilahmedrimel002@gmail.com>
2026-09-18 22:18:48 +0600

    update something

--- day30.py
+++ day30.py
@@ -80,3 +80,10 @@ if "apple" in fruit:
     print("found")
 else:
     print("not found")
+
+num=[1,2,3,4,5,2,1,1,2,3,1]
+for i in num:
+    if i==1:
+        print("found")
+    else:
+        print("not found")
